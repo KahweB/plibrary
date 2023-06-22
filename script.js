919d1a125f7b3ca2456560c1351d5f602b495033
Kahve <72060765+KahweB@users.noreply.github.com>
2023-06-22 03:11:42 +0300

    add: popup close

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ const newbook_Button = document.getElementById("newbook_button");
 
 
 
-
-
-//event listener of top button
+//event listener top button
 newbook_Button.addEventListener("click",createPopup);
+
 
 //constructor
 function Book(title, author, pages, read) {
@@ -20,15 +19,39 @@ function Book(title, author, pages, read) {
 function createPopup(){
     const popupDiv = document.createElement("div");
     popupDiv.setAttribute("id","popup");
-
+    
     const form = document.createElement("form");
     form.setAttribute("id", "bookForm");
 
     const inputTitle = document.createElement("input");
-    inputTitle.setAttribute("id","inputTitle");
-    inputTitle.setAttribute("type","text");
-    
-    
+    inputTitle.setAttribute("id", "inputTitle");
+    inputTitle.setAttribute("type", "text");
+
+    const inputAuthor =  document.createElement("input");
+    inputAuthor.setAttribute("id", "inputAuthor");
+    inputAuthor.setAttribute("type","text");
+
+    const inputPages = document.createElement("input");
+    inputPages.setAttribute("id", "inputPages");
+    inputPages.setAttribute("type", "numher");
+
+    const inputRead = document.createElement("input");
+    inputRead.setAttribute("id", "inputRead");
+    inputRead.setAttribute("type", "text");
+
+    const closePopupButn = document.createElement("button");
+    closePopupButn.setAttribute("id", "closePopup");
+    closePopupButn.innerHTML = "Close";
+
+    closePopupButn.addEventListener("click", function(){
+        popupDiv.remove();
+    });
+
     container.appendChild(popupDiv);
     popupDiv.appendChild(form);
+    form.appendChild(inputTitle);
+    form.appendChild(inputAuthor);
+    form.appendChild(inputPages);
+    form.appendChild(inputRead);
+    popupDiv.appendChild(closePopupButn);
 }
